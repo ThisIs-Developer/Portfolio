9cf2a9fe9d6cb0daf262f50cdb882fd8c91913a3
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-05-08 10:49:37 +0530

    Add Clarity Script

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "i4u605tz2t");
